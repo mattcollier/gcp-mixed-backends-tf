de26b2ad660d9f995627de5f9cfb7092978e016c
Matthew Collier <collier.matthew@gmail.com>
2025-07-19 12:15:42 -0400

    Update version constraint for cloud shell.

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # providers.tf
 ############################################
 terraform {
-  required_version = ">= 1.6"
+  required_version = ">= 1.5.7"
   required_providers {
     google      = { source = "hashicorp/google", version = "~> 5.28" }
     google-beta = { source = "hashicorp/google-beta", version = "~> 5.28" }
